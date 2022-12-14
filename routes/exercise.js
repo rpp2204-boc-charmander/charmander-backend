aecc30d563ec42828f2b7d2dbb02db0a492bcef0
Riccardo Pirruccio <rickp1795@gmail.com>
2022-12-13 22:17:48 -0600

    updated queries and routes

--- routes/exercise.js
+++ routes/exercise.js
@@ -14,14 +14,14 @@ const {
 
 //add more sub routes here. See app.js for main route
 router.get('/default/list', getDefaultExercises);
-router.get('/custom/list', getUserExercises);
+router.get('/custom/list', getUserExercises); //query params: username
 
 //take care of workout exercises
-router.post('/create', postUserWorkoutExercise);
-router.get('/list', getUserWorkoutForDate);
+router.post('/create', postUserWorkoutExercise); //query params: username, log_date, exercise
+router.get('/list', getUserWorkoutForDate); //query params: username, log_date
 
 //take care of sets
-router.post('/create/set', postExerciseSet);
-router.get('/list/sets', getExerciseSet);
+router.post('/create/set', postExerciseSet); //query params: weight_lbs, reps, workout_exercise_id
+router.get('/list/sets', getExerciseSet); //query params: workout_exercise_id
 
 module.exports = router;
