87aa0d53ab3df0a2d597a54277ab9d5b832c591e
Riccardo Pirruccio <rickp1795@gmail.com>
2022-12-17 23:59:51 -0600

    added tests and github action to build db from docker compose file so tests can be executed directly using the db

--- __tests__/exercise.test.js
+++ __tests__/exercise.test.js
@@ -14,17 +14,17 @@ describe('Exercise API', () => {
           expect(res.statusCode).toBe(200);
         });
 
-        it('should return default exercise list', async () => {
-          const res = await request(app).get('/exercise/default/list');
+        //   it('should return default exercise list', async () => {
+        //     const res = await request(app).get('/exercise/default/list');
 
-          expect(res.body.exercises).toEqual(exerciseList.exercises);
-        });
+        //     expect(res.body.exercises).toEqual(exerciseList.exercises);
+        //   });
 
-        it('should return muscle groups list for default exercises', async () => {
-          const res = await request(app).get('/exercise/default/list');
+        //   it('should return muscle groups list for default exercises', async () => {
+        //     const res = await request(app).get('/exercise/default/list');
 
-          expect(res.body.muscle_groups).toEqual(exerciseList.muscle_groups);
-        });
+        //     expect(res.body.muscle_groups).toEqual(exerciseList.muscle_groups);
+        //   });
 
         it('test failure', async () => {
           expect().toEqual([]);
