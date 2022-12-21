15884541e79f269ad95b5a88db927e140670bff6
Keenan Aldridge <kmantan@gmail.com>
2022-12-20 17:08:30 -1000

    Added nutrition schema, routes and started model / controllers

--- model/nutrition.js
+++ model/nutrition.js
@@ -1,3 +1,16 @@
-const { query } = require('../db/index');
+const db = require('../db');
 
-module.exports = {};
+module.exports = {
+  addUserFoods: async() => {
+    // add foods insert will go here
+  },
+  getUserFoods: async() => {
+    // get foods for the day query will go here
+  },
+  updateUserFoods: async() => {
+    // update foods put will go here
+  },
+  removeUserFoods: async() => {
+    // delete foods will go here
+  }
+};
--- routes/nutrition.js
+++ routes/nutrition.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const controller = require('../controllers');
 
 //add your controllers to this object
-const { test } = controller.nutrition;
+const {
+  getUserFoods,
+  addUserFoods,
+  updateUserFoods,
+  removeUserFoods
+} = controller.nutrition;
 
 //add more sub routes here. See app.js for main route
-router.get('/test', test);
+router.get('/list/foods', getUserFoods);
+
+router.post('/create/foods', addUserFoods);
+
+router.put('/update/foods', updateUserFoods);
+
+router.delete('/remove/foods', removeUserFoods);
 
 module.exports = router;
