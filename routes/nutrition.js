b3dd55c835cff556b80deea2aafd1f46da342af7
Keenan Aldridge <kmantan@gmail.com>
2022-12-26 19:40:40 -1000

    Updated nutrition schemas

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -11,12 +11,12 @@ const {
 } = controller.nutrition;
 
 //add more sub routes here. See app.js for main route
-router.get('/list/foods', getUserFoods);
+// router.get('/list/foods', getUserFoods);
 
 router.post('/create/foods', addUserFoods);
 
-router.put('/update/foods', updateUserFoods);
+// router.put('/update/foods', updateUserFoods);
 
-router.delete('/remove/foods', removeUserFoods);
+// router.delete('/remove/foods', removeUserFoods);
 
 module.exports = router;
