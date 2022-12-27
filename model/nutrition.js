40987fb6dbd393c400723d7b29b6d4ac34af837e
Keenan Aldridge <kmantan@gmail.com>
2022-12-26 19:49:30 -1000

    Added nutrition schema, routes and started model / controllers

--- model/nutrition.js
+++ model/nutrition.js
@@ -1,3 +1,30 @@
-const { query } = require('../db/index');
+const db = require('../db');
 
-module.exports = {};
+module.exports = {
+  addUserFoodsToDB: async() => {
+    // add foods insert will go here
+
+    const queryString = `INSERT INTO public.nutrition_log(
+      weight_lbs, reps, workout_exercise_id)
+      VALUES ($1, $2, $3)`;
+
+    const params = [weight_lbs, reps, workout_exercise_id];
+
+    try {
+      const result = await query(queryString, params);
+
+      return result.rows;
+    } catch (err) {
+      throw err;
+    }
+  },
+  getUserFoodsFromDB: async() => {
+    // get foods for the day query will go here
+  },
+  updateUserFoodsInDB: async() => {
+    // update foods put will go here
+  },
+  removeUserFoodsInDB: async() => {
+    // delete foods will go here
+  }
+};
