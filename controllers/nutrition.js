4ae98295f6fa0af49237c7c6012140346d68aee7
Keenan Aldridge <kmantan@gmail.com>
2022-12-27 19:54:49 -1000

    Completed GET foods for nutrition

--- controllers/nutrition.js
+++ controllers/nutrition.js
@@ -2,35 +2,64 @@
 //make sure you do error handling like so
 //if you take care of errors like the below example, the error middleware will take care of the error for you
 const {
+  addFoodsToDB,
+  addUserFoods,
   getUserFoodsFromDB,
-  addUserFoodsToDB,
-  updateUserFoodsInDB,
-  removeUserFoodsInDB
+  updateUserFoods,
+  removeUserFoods,
+  foodSearchInDB
 } = require('../model/nutrition');
 
 module.exports = {
-  getUserFoods: (req, res, next) => {
+  foodSearch: async (req, res, next) => {
+    const { food_id } = req.query;
+    try {
+      const id = await foodSearchInDB(food_id);
+      if(id.length === 0){
+        const foodId = await module.exports.addFoodToDB(req, res, next);
+        res.send(foodId);
+      } else {
+        res.status(200).send(id[0]);
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+  getFoodLog: async (req, res, next) => {
     try {
       res.send('hi');
     } catch (err) {
       next(err);
     }
   },
-  addUserFoods: (req, res, next) => {
+  logFood: async (req, res, next) => {
     try {
       res.send('hi');
     } catch (err) {
       next(err);
     }
   },
-  updateUserFoods: (req, res, next) => {
+  addFoodToDB: async (req, res, next) => {
+    const { label, foodId, nutrients } = req.query.food;
+    const stringedMeasurements = JSON.stringify(req.query.measures);
+    const stringedNutrients = JSON.stringify(nutrients);
+    const food_image = req.query.food.food_image || 'no image available';
+    try {
+      const newId = await addFoodsToDB(label, foodId, stringedMeasurements, stringedNutrients, food_image);
+      console.log('newId: ', newId);
+      res.send(newId);
+    } catch (err) {
+      next(err);
+    }
+  },
+  updateLog: async (req, res, next) => {
     try {
       res.send('hi');
     } catch (err) {
       next(err);
     }
   },
-  removeUserFoods: (req, res, next) => {
+  deleteFromLog: async (req, res, next) => {
     try {
       res.send('hi');
     } catch (err) {
--- model/nutrition.js
+++ model/nutrition.js
@@ -1,59 +1,101 @@
 const db = require('../db');
 
 module.exports = {
-  addUserFoodsToDB: async() => {
-    // add foods insert will go here
+  addFoodsToDB: async (food_name, food_id, measurements, nutrients, food_image) => {
+    // adds food to DB if it does not exist
 
-    const queryString = `INSERT INTO public.nutrition_log(
-      weight_lbs, reps, workout_exercise_id)
-      VALUES ($1, $2, $3)`;
+    const queryString = `INSERT INTO public.foods(
+      food_name, food_id, measurements, nutrients, food_image)
+      VALUES ($1, $2, $3, $4, $5);`;
 
-    const params = [weight_lbs, reps, workout_exercise_id];
-
+    const params = [food_name, food_id, measurements, nutrients, food_image];
     try {
-      const result = await query(queryString, params);
-const db = require('../db');
+      await db.query(queryString, params);
+      const newId = await module.exports.foodSearchInDB(food_id)
+      return newId;
 
-module.exports = {
-  addUserFoods: async() => {
-    // add foods insert will go here
-
-    const queryString = `INSERT INTO public.nutrition_log(
-      weight_lbs, reps, workout_exercise_id)
-      VALUES ($1, $2, $3)`;
-
-    const params = [weight_lbs, reps, workout_exercise_id];
-
-    try {
-      const result = await query(queryString, params);
-
-      return result.rows;
     } catch (err) {
       throw err;
     }
   },
-  getUserFoodsFromDB: async() => {
-    // get foods for the day query will go here
-  },
-  updateUserFoodsInDB: async() => {
-    // update foods put will go here
-  },
-  removeUserFoodsInDB: async() => {
-    // delete foods will go here
-  }
-};
+  addUserFoods: async (user_id, log_date, food, portion, consumed) => {
+    // logs food
+
+    const queryString = `INSERT INTO public.nutrition_log(
+      user_id, log_date, food, portion, consumed)
+      VALUES ($1, $2, $3, $4, $5);`;
+
+    const params = [user_id, log_date, food, portion, consumed];
+
+    try {
+      const result = await db.query(queryString, params);
       return result.rows;
+
     } catch (err) {
       throw err;
     }
   },
-  getUserFoods: async() => {
-    // get foods for the day query will go here
+  getUserFoodsFromDB: async (user_id, log_date) => {
+    // gets user's food log
+    const queryString = `SELECT nlog.id, nlog.portion, nlog.food, nlog.consumed, foodinfo.food_name, foodinfo.nutrients, foodinfo.food_image
+      FROM public.nutrition_log nlog
+      JOIN public.foods foodinfo
+      ON nlog.food = foodinfo.id
+      WHERE nlog.user_id = $1 AND nlog.log_date = $2;`;
+
+    const params = [user_id, log_date];
+
+    try {
+      const result = await db.query(queryString, params);
+      return result.rows;
+
+    } catch (err) {
+      throw err;
+    }
   },
-  updateUserFoods: async() => {
-    // update foods put will go here
+  updateUserFoods: async (logId, consumedStatus, portion) => {
+    // updates a food for user's food log
+    const queryString = `UPDATE public.nutrition_log
+      SET consumed = $2, portion = $3
+      WHERE id = $1;`;
+
+    const params = [logId, consumedStatus, portion];
+
+    try {
+      const result = await db.query(queryString, params);
+      return result.rows;
+
+    } catch (err) {
+      throw err;
+    }
   },
-  removeUserFoods: async() => {
-    // delete foods will go here
+  removeUserFoods: async (logId) => {
+    // delete a food from user's food log
+    const queryString = `DELETE from public.nutrition_log WHERE id = $1;`;
+
+    const params = [logId];
+
+    try {
+      const result = await db.query(queryString, params);
+      return result.rows;
+
+    } catch (err) {
+      throw err;
+    }
+  },
+  foodSearchInDB: async (food_id) => {
+    // will see if DB has the food
+    const queryString = `SELECT id FROM public.foods
+      WHERE food_id = $1;`;
+
+    const params = [food_id];
+
+    try {
+      const result = await db.query(queryString, params);
+      return result.rows;
+
+    } catch (err) {
+      throw err;
+    }
   }
 };
