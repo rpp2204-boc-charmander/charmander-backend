953ef6fb39b1dae3344e09e79c54bd4046023f7e
ChiElephant <anthony.merino@me.com>
2022-12-30 11:24:40 -0700

    added routes/controllers/models to get user by email and user by user_id

--- model/user.js
+++ model/user.js
@@ -9,11 +9,12 @@ module.exports = {
     user_password,
     weight_lbs,
     height_inches,
-    sex
+    sex,
+    profile_pic
   ) => {
     const queryString = `INSERT INTO public.users(
       auth_id, firstname, lastname, email, user_password, weight_lbs, height_inches, sex)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
     const params = [
       auth_id,
       firstname,
@@ -23,9 +24,11 @@ module.exports = {
       weight_lbs,
       height_inches,
       sex,
+      profile_pic,
     ];
     try {
       const result = await query(queryString, params);
+      console.log('params', params)
 
       return result.rows;
     } catch (err) {
@@ -33,8 +36,8 @@ module.exports = {
     }
   },
 
-  selectUserFromDB: async (auth_id) => {
-    const queryString = `SELECT id AS user_id, auth_id, firstname, lastname, email, user_password, weight_lbs, height_inches, sex
+  checkUserInDB: async (auth_id) => {
+    const queryString = `SELECT id AS user_id, auth_id, firstname, lastname, email, user_password, weight_lbs, height_inches, sex, profile_pic
     FROM public.users
     WHERE auth_id=$1`;
 
@@ -42,10 +45,41 @@ module.exports = {
 
     try {
       const result = await query(queryString, params);
-
       return result.rows[0];
     } catch (err) {
       throw err;
     }
   },
+
+  checkEmailInDB: async (email) => {
+    const queryString = `SELECT id AS user_id, auth_id, firstname, lastname, email, user_password, weight_lbs, height_inches, sex, profile_pic
+    FROM public.users
+    WHERE email=$1`;
+
+    const params = [email];
+
+    try {
+      const result = await query(queryString, params);
+      return result.rows[0];
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  selectUserFromDB: async (user_id) => {
+    const queryString = `SELECT id AS user_id, auth_id, firstname, lastname, email, user_password, weight_lbs, height_inches, sex, profile_pic
+    FROM public.users
+    WHERE user_id=$1`;
+
+    const params = [user_id];
+
+    try {
+      const result = await query(queryString, params);
+      return result.rows[0];
+    } catch (err) {
+      throw err;
+    }
+  },
+
+
 };
