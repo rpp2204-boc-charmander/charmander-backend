4ba7567282f21a6fcdbb3550aa0e0bfd05e357b7
Keenan Aldridge <kmantan@gmail.com>
2022-12-30 14:28:20 -1000

    Added routes for nutrition

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -4,19 +4,23 @@ const controller = require('../controllers');
 
 //add your controllers to this object
 const {
-  getUserFoods,
-  addUserFoods,
-  updateUserFoods,
-  removeUserFoods
+  foodSearch,
+  getFoodLog,
+  logFood,
+  addFoodToDB,
+  updateLog,
+  deleteFromLog
 } = controller.nutrition;
 
 //add more sub routes here. See app.js for main route
-// router.get('/list/foods', getUserFoods);
+router.get('/list/foods', foodSearch);
 
-router.post('/create/foods', addUserFoods);
+router.get('/list/foodLog', getFoodLog);
 
-// router.put('/update/foods', updateUserFoods);
+router.post('/create/logFoods', logFood);
 
-// router.delete('/remove/foods', removeUserFoods);
+router.put('/update/foodLog', updateLog);
+
+router.delete('/remove/foodLog', deleteFromLog);
 
 module.exports = router;
